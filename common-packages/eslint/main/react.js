c4077430f7c3d03be6790b37eda1643e0f370215
theGobindSingh <thesinghgobind@gmail.com>
2026-03-28 13:58:34 +0000

    init

--- common-packages/eslint/main/react.js
+++ common-packages/eslint/main/react.js
@@ -1,46 +1,24 @@
-const pluginReact = require("eslint-plugin-react");
+const eslintReact = require("@eslint-react/eslint-plugin");
 const pluginReactHooks = require("eslint-plugin-react-hooks");
 const pluginReactRefresh = require("eslint-plugin-react-refresh");
 const baseConfig = require("./index.js");
-const preferFunctionComponentConfig = require("eslint-plugin-react-prefer-function-component/config");
 
 /** @type {import("eslint").Linter.Config[]} */
 const config = [
   ...baseConfig,
-  pluginReact.configs.flat.recommended,
+  eslintReact.configs["recommended-typescript"],
   pluginReactHooks.configs.flat["recommended-latest"],
   pluginReactRefresh.configs.recommended,
-  preferFunctionComponentConfig.configs.recommended,
   {
     rules: {
-      ...pluginReact.configs.flat?.recommended?.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "react/jsx-indent-props": ["warn", 2],
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-props-no-spreading": "off",
-      "react/prop-types": "off",
-      "react/require-default-props": "off",
       "react-hooks/exhaustive-deps": "warn",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/set-state-in-effect": "off",
-      "react/no-unknown-property": ["error", { ignore: ["css"] }],
-      "react/function-component-definition": [
-        "warn",
-        {
-          namedComponents: "arrow-function",
-          unnamedComponents: "arrow-function",
-        },
-      ],
-      "react-prefer-function-component/react-prefer-function-component": [
-        "error",
-        {
-          allowErrorBoundary: true,
-          allowJsxUtilityClass: false,
-        },
-      ],
+      "@eslint-react/dom/no-unknown-property": ["error", { ignore: ["css"] }],
     },
   },
 ];
--- common-packages/eslint/main/react.mjs
+++ common-packages/eslint/main/react.mjs
@@ -1,47 +1,25 @@
-import pluginReact from "eslint-plugin-react";
+import eslintReact from "@eslint-react/eslint-plugin";
 import pluginReactHooks from "eslint-plugin-react-hooks";
-import preferFunctionComponentConfig from "eslint-plugin-react-prefer-function-component/config";
 import pluginReactRefresh from "eslint-plugin-react-refresh";
 import baseConfig from "./index.mjs";
-// import preferFunctionComponentPlugin from "eslint-plugin-react-prefer-function-component";
 
 /** @type {import("eslint").Linter.Config[]} */
 const config = [
   ...baseConfig,
-  pluginReact.configs.flat.recommended,
+  eslintReact.configs["recommended-typescript"],
   pluginReactHooks.configs.flat["recommended-latest"],
   pluginReactRefresh.configs.recommended,
-  preferFunctionComponentConfig.configs.recommended,
   {
     rules: {
-      ...pluginReact.configs.flat?.recommended?.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "react/jsx-indent-props": ["warn", 2],
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-props-no-spreading": "off",
-      "react/prop-types": "off",
-      "react/require-default-props": "off",
       "react-hooks/exhaustive-deps": "warn",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/set-state-in-effect": "off",
-      "react/no-unknown-property": ["error", { ignore: ["css"] }],
-      "react/function-component-definition": [
-        "warn",
-        {
-          namedComponents: "arrow-function",
-          unnamedComponents: "arrow-function",
-        },
-      ],
-      "react-prefer-function-component/react-prefer-function-component": [
-        "error",
-        {
-          allowErrorBoundary: true,
-          allowJsxUtilityClass: false,
-        },
-      ],
+      "@eslint-react/set-state-in-effect": "off",
+      "@eslint-react/dom/no-unknown-property": ["error", { ignore: ["css"] }],
     },
   },
 ];
